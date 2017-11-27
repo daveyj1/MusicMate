65c3ee5b526292f7b99ce453ff5e1464dca12733
jdidat <jdidat@purdue.edu>
2017-11-27 01:39:17 -0500

    Finished beautiful and fresh UI design, part 1 of 3 almost finished

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+var express = require('express'); // Express web server framework
+var request = require('request'); // "Request" library
+var querystring = require('querystring');
+var cookieParser = require('cookie-parser');
+
+var client_id = '9e91dbd172c24b608a18276182a66308'; // my client id
+var client_secret = '150e886419e04d43ad8fd06225d0a56d'; // my secret key
+var redirect_uri = 'http://localhost:8888/callback'; // my redirect uri
+
 function buttonClick() {
     alert("Joe has big titties");
 }
