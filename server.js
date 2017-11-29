3bf8a0c5e075b4f3e12dcb702f35857f94a1836f
jdidat <jdidat@purdue.edu>
2017-11-29 03:45:27 -0500

    added package.json

---
+++ server.js
@@ -0,0 +1,68 @@
+let express = require('express');
+let bodyParser = require('body-parser');
+let cors = require('cors');
+let request = require('request');
+let SpotifyWebApi = require('spotify-web-api-node');
+let client_id = '9e91dbd172c24b608a18276182a66308';
+let client_secret = '150e886419e04d43ad8fd06225d0a56d';
+let spotifyApi = new SpotifyWebApi({
+    clientId: client_id,
+    clientSecret: client_secret,
+    redirectUri: 'http://localhost:8000/'
+});
+let app = express();
+app.use("/css", express.static(__dirname + '/static/css'));
+app.use("/js", express.static(__dirname + '/static/js'));
+app.use(cors());
+app.use(bodyParser.json());
+app.get('/', (req, res) => {
+    let payload = client_id + ":" + client_secret;
+    let encodedPayload = new Buffer(payload).toString("base64");
+    let opts = {
+        url: "https://accounts.spotify.com/api/token",
+        method: "POST",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Authorization": "Basic " + encodedPayload
+        },
+        body: "grant_type=client_credentials&scope=playlist-modify-public playlist-modify-private"
+    };
+    request(opts, function (err, res, body) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            console.log(JSON.parse(body).access_token);
+            spotifyApi.setAccessToken(JSON.parse(body).access_token);
+        }
+    });
+
+    res.sendfile('./static/index.html');
+});
+
+app.post('/searchArtist', (request, response) => {
+    let errorFlag = false;
+    let dataArray = [];
+    let artistsObject = request.body;
+    let count = Object.keys(artistsObject).length;
+    for (let key in artistsObject) {
+        if (artistsObject.hasOwnProperty(key))
+            spotifyApi.searchArtists(artistsObject[key])
+                .then(function (data) {
+                    dataArray.push(data);
+                    count--;
+                    if (count <= 0 && !errorFlag) {
+                        return response.status(200).json({
+                            dataArray
+                        });
+                    }
+                }, function (err) {
+                    errorFlag = true;
+                    return response.status(400).json({
+                        message: err
+                    });
+                });
+    }
+});
+
+app.listen(8000);
--- static/js/index.js
+++ static/js/index.js
@@ -1,45 +1,71 @@
-var client_id = '9e91dbd172c24b608a18276182a66308'; // my client id
-var client_secret = '150e886419e04d43ad8fd06225d0a56d'; // my secret key
-var redirect_uri = 'http://localhost:8888/callback'; // my redirect uri
+// var client_id = '9e91dbd172c24b608a18276182a66308'; // my client id
+// var client_secret = '150e886419e04d43ad8fd06225d0a56d'; // my secret key
+// var redirect_uri = 'http://localhost:8888/callback'; // my redirect uri
 
-const app = {};
-
-app.apiUrl = 'https://api.spotify.com/v1';
-
-app.events = function() {
-    $('form').on('submit', function(e) {
-        e.preventDefault();
-        var genreOne = document.getElementById("firstGenre").value;
-        var genreTwo = document.getElementById("secondGenre").value;
-        var genreThree = document.getElementById("thirdGenre").value;
-        console.log(genreOne);
-        console.log(genreTwo);
-        console.log(genreThree);
-        var genreArray = [genreOne, genreTwo, genreThree];
-        let search = genreArray.map(genreName => app.searchGenres(genreName));
-        console.log(search);
-        $.when(...search)
-            .then((...results) => {
-                console.log(results);
-            });
-
-    });
-};
-
-app.searchGenres = (genreName) => $.ajax({
-    url: `${app.apiUrl}/artists`,
-    method: 'GET',
-    dataType: 'json',
-    data: {
-        q: genreName,
-        type: 'artist'
+// const app = {};
+//
+// app.apiUrl = 'https://api.spotify.com/v1';
+//
+// app.events = function() {
+//     $('form').on('submit', function(e) {
+//         e.preventDefault();
+//         var genreOne = document.getElementById("firstGenre").value;
+//         var genreTwo = document.getElementById("secondGenre").value;
+//         var genreThree = document.getElementById("thirdGenre").value;
+//         console.log(genreOne);
+//         console.log(genreTwo);
+//         console.log(genreThree);
+//         var genreArray = [genreOne, genreTwo, genreThree];
+//         let search = genreArray.map(genreName => app.searchGenres(genreName));
+//         console.log(search);
+//         $.when(...search)
+//             .then((...results) => {
+//                 console.log(results);
+//             });
+//
+//     });
+// };
+//
+// app.searchGenres = (genreName) => $.ajax({
+//     url: `${app.apiUrl}/artists`,
+//     method: 'GET',
+//     dataType: 'json',
+//     data: {
+//         q: genreName,
+//         type: 'artist'
+//     }
+// });
+//
+//
+// app.init = function() {
+//     app.events();
+// };
+//
+// $(app.init);
+function getArtists() {
+    let artists = {};
+    let artist1 = $('#firstGenre').val();
+    if(artist1 !== "") artists.artist1 = artist1;
+    let artist2 = $('#secondGenre').val();
+    if(artist2 !== "") artists.artist2 = artist2;
+    let artist3 = $('#thirdGenre').val();
+    if(artist3 !== "") artists.artist3 = artist3;
+    if(jQuery.isEmptyObject(artists)){
+        alert("Please enter at least 1 artist.");
+        return;
     }
-});
-
-
-app.init = function() {
-    app.events();
-};
-
-$(app.init);
-
+    alert(JSON.stringify(artists));
+    $.ajax({
+        url: '/searchArtist',
+        dataType: 'json',
+        type: 'POST',
+        data: JSON.stringify(artists),
+        contentType: "application/json",
+        success: function (data) {
+            console.log(JSON.stringify(data));
+        },
+        error: function (err) {
+            console.log(err);
+        }
+    });
+}
