934eb918265abf61f1d092e38802a8b8fd57a894
jdidat <jdidat@purdue.edu>
2017-12-06 16:31:33 -0500

    updates on styling

--- static/js/index.js
+++ static/js/index.js
@@ -174,9 +174,9 @@ function showDiv(songArray) {
     //document.getElementById('playlistArtists').innerHTML = "";
     for (var i = 0; i < 15; i++) {
         let song = songArray[i].substring(songArray[i].indexOf("+") + 1, songArray[i].length);
-        let artist = songArray[i].substring(0, songArray[i].indexOf("+"));
+        //let artist = songArray[i].substring(0, songArray[i].indexOf("+"));
         document.getElementById('playlistNames').innerHTML += '<i class="fa fa-play-circle" style="font-size:24px;"></i> ' + song + "<br />";
-        document.getElementById('playlistArtists').innerHTML += artist + "<br />";
+        //document.getElementById('playlistArtists').innerHTML += artist + "<br />";
     }
 }
 
